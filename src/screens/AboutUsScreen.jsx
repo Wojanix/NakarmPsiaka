2dfa31eb8b18da355479f836981016fc90e2bae7
rudy282 <121287489+rudy282@users.noreply.github.com>
2023-10-28 19:42:04 +0200

    Created project structure, added color variables and navigation

---
+++ src/screens/AboutUsScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const AboutUsScreen = () => {
+	return (
+		<View>
+			<Text>AboutUsScreen</Text>
+		</View>
+	);
+};
+export default AboutUsScreen;
