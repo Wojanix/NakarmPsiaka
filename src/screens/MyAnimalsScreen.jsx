9f1a50e031e83698d587a06aea24fe0f7bddab5a
rudy282 <121287489+rudy282@users.noreply.github.com>
2023-11-01 23:57:47 +0100

    Added reanimated dependency, created skeleton animation for MyAnimalsScreen.

--- src/screens/MyAnimalsScreen.jsx
+++ src/screens/MyAnimalsScreen.jsx
@@ -1,19 +1,42 @@
-import React from "react";
-import { FlatList, SafeAreaView, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+	FlatList,
+	SafeAreaView,
+	StyleSheet,
+	TouchableWithoutFeedback,
+	View,
+} from "react-native";
 import Animal from "../components/MyAnimalsComponents/Animal";
 import { COLOR_MAIN_BG } from "../constants/colors";
 import { animalData } from "../constants/testData";
+import SkeletonView from "../components/MyAnimalsComponents/SkeletonView";
 
 const MyAnimalsScreen = () => {
 	const { container } = styles;
 
+	// * only for test purposes, it will be set by fetch hook in future
+	// * the TouchableWithoutFeedback, has been added only for test purposes too
+	const [isLoading, setIsLoading] = useState(true);
+
 	return (
 		<SafeAreaView style={container}>
-			<FlatList
-				data={animalData}
-				keyExtractor={(item) => item.id}
-				renderItem={({ item }) => <Animal {...item} />}
-			/>
+			{isLoading ? (
+				<TouchableWithoutFeedback
+					onPress={() => setIsLoading(!isLoading)}>
+					<View>
+						<SkeletonView />
+						<SkeletonView />
+						<SkeletonView />
+						<SkeletonView />
+					</View>
+				</TouchableWithoutFeedback>
+			) : (
+				<FlatList
+					data={animalData}
+					keyExtractor={(item) => item.id}
+					renderItem={({ item }) => <Animal {...item} />}
+				/>
+			)}
 		</SafeAreaView>
 	);
 };
