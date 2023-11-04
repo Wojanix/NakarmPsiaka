d5c22f280cb376979415d9ac448384d70d5bd361
Wojanix <wojtek.burek@gmail.com>
2023-11-04 18:40:22 +0100

    First of 2 Commits Settings
    Jeszcze pressable i tlo ladne

---
+++ src/components/Settings/SettingRecord.tsx
@@ -0,0 +1,82 @@
+import { View, Text, StyleSheet, Switch, Pressable } from "react-native";
+import React, { useState } from "react";
+import {
+  COLOR_MAIN_BG,
+  COLOR_MAIN_TEXT,
+  COLOR_PRIMARY,
+  COLOR_SECONDARY_TEXT,
+} from "../../constants/colors";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons"; // info button
+import { MaterialIcons } from "@expo/vector-icons"; // edit button
+import { FontAwesome } from "@expo/vector-icons"; // remove button
+
+export default function SettingRecord(props) {
+  const { text, title, icon, som, index, iconSize, argFunction } = props;
+  argFunction ? argFunction() : "";
+
+  console.log(argFunction);
+
+  const [isEnabled, setIsEnabled] = useState(som);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  return (
+    <Pressable
+      style={styles.container}
+      //   onPress={() => (argFunction ? argFunction() : "")}
+    >
+      <Text style={styles.icon}>
+        <FontAwesome
+          name={icon}
+          size={iconSize ? iconSize : 38}
+          color={"black"}
+        />
+      </Text>
+      <View style={styles.text}>
+        <Text style={styles.textTop}>{title}</Text>
+        <Text style={styles.textBottom}>{text}</Text>
+      </View>
+
+      {som ? (
+        <Switch
+          style={styles.som}
+          trackColor={{ false: "#767577", true: COLOR_SECONDARY_TEXT }}
+          thumbColor={isEnabled ? COLOR_PRIMARY : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      ) : (
+        ""
+      )}
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    borderColor: "black",
+    borderTopWidth: 0,
+    padding: 15,
+  },
+  icon: {
+    width: "12%",
+  },
+  text: {
+    width: "62%",
+  },
+  textTop: {
+    fontSize: 20,
+    color: COLOR_MAIN_TEXT,
+  },
+  textBottom: {
+    fontSize: 14,
+    color: COLOR_SECONDARY_TEXT,
+  },
+  som: {
+    width: "20%",
+    transform: [{ scale: 1.3 }],
+  },
+});
--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SettingRecord from "../components/Settings/SettingRecord";
+import { COLOR_MAIN_BG } from "../constants/colors";
 
-import { View, Text } from "react-native";
+
 const SettingsScreen = () => {
+
+	const writeOutSomething = () =>{
+		console.log("Guzik wcisniety")
+	}
+
+	const settingsArray = [
+		{title:"Allow localization", text: "Allows us to sell your soul", icon: "map-pin", som: true},
+		{title:"Allow localization", text: "Allows us to sell your soul", icon: "map-pin", som: true},
+		{title:"Allow everything", text: "Allows us to fuck u rmom", icon: "map-pin", som: false},
+		{title:"Allow everything", text: "Allows us to fuck u rmom", icon: "map-pin", som: true},
+		{title:"Remove my data", text: "Your data won't be removed :D", icon: "trash", iconSize: 32, som: true},
+		{title:"Remove my data", text: "Your data won't be removed :D", icon: "trash", iconSize: 32, som: false, argFunction: ()=>writeOutSomething()},
+		{title:"Remove my data", text: "Your data won't be removed :D", icon: "trash", iconSize: 32, som: false, argFunction: ()=>writeOutSomething()},
+	]
+
+	// settingsArray.map((rec)=>{console.log(rec.icon)})
 	return (
-		<View>
-			<Text>SettingsScreen</Text>
+		<View >
+			{settingsArray.map((rec, index)=>(
+				<SettingRecord title={rec.title}  text={rec.text}  icon={rec.icon}  som={rec.som} index={index} iconSize={rec.iconSize}/>
+			))}
 		</View>
 	);
 };
 export default SettingsScreen;
+
