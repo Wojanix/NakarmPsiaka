b5a15a4178e700512316bb4fb1631d3e17a260cd
rudy282 <121287489+rudy282@users.noreply.github.com>
2023-11-27 21:15:34 +0100

    Animal menu is not a component, now is an modal, it doesn't trigger rerender

--- src/components/MapComponents/AnimalMenu.jsx
+++ src/components/MapComponents/AnimalMenu.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { COLOR_MAIN_BG } from "../../constants/colors";
 
-const AnimalMenu = ({ animal, setShowMap, showMap }) => {
+const AnimalMenu = ({ animal, setShowAnimalModal, showAnimalModal }) => {
+	const { id, type, name, breed, info } = animal;
+
 	return (
-		<View>
-			<Text>AnimalMenu</Text>
-			<Button title="go back" onPress={() => setShowMap(!showMap)} />
+		<View style={styles.container}>
+			<Text>{id}</Text>
+			<Text>{name}</Text>
+			<Text>{type}</Text>
+			<Text>{breed}</Text>
+			<Text>{info}</Text>
+			<Button title="nakarm" />
+			<Button
+				title="wróć"
+				onPress={() => setShowAnimalModal(!showAnimalModal)}
+			/>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: COLOR_MAIN_BG,
+	},
+});
+
 export default AnimalMenu;
