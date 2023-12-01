b7f68cddd06dcab2b889de91cd31b6a454a9c000
rudy282 <121287489+rudy282@users.noreply.github.com>
2023-12-01 21:54:52 +0100

    Created Java Spring Boot application, which will be the server of Nakarm Psiaka

---
+++ Backend/nakarmPsiaka/src/test/java/com/example/nakarmpsiaka/NakarmPsiakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nakarmpsiaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NakarmPsiakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
